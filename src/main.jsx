df24a7fc9e20f78a222194d11c1e1a193b3a3c77
Lidia Michalska <144784986+lmichalska@users.noreply.github.com>
2024-10-20 00:21:22 +0200

    1

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/assignment/"}>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc"; 
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+    const config = {
+        plugins: [react()],
+        base: "/"
+    };
+
+    if (command !== "serve") {
+        config.base = "/assignment";
+    }
+
+    return config;
+});
